b9e77a935d70605ba16a889e8b7c1f4718406219
Michal Smaga <msmaga@duckduckgo.com>
2026-02-26 11:03:33 +0100

    Make web extension context conditionally inspectable

--- SharedPackages/WebExtensions/Sources/WebExtensions/Loading/WebExtensionLoader.swift
+++ SharedPackages/WebExtensions/Sources/WebExtensions/Loading/WebExtensionLoader.swift
@@ -52,10 +52,12 @@ public final class WebExtensionLoader: WebExtensionLoading {
     }
 
     private let storageProvider: WebExtensionStorageProviding
+    private let isInspectable: Bool
     public weak var delegate: WebExtensionLoadingDelegate?
 
-    public init(storageProvider: WebExtensionStorageProviding) {
+    public init(storageProvider: WebExtensionStorageProviding, isInspectable: Bool = false) {
         self.storageProvider = storageProvider
+        self.isInspectable = isInspectable
     }
 
     @MainActor
@@ -137,8 +139,7 @@ public final class WebExtensionLoader: WebExtensionLoading {
             context.setPermissionStatus(.grantedExplicitly, for: permission, expirationDate: nil)
         }
 
-        // For debugging purposes
-        context.isInspectable = true
+        context.isInspectable = isInspectable
         return context
     }
 }
